f6acfcede722668f17e6055fc72539ccdf177fee
CL <cl.czayas@gmail.com>
2023-01-04 17:54:37 -0400

    Connected database and started prompts

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 const inquirer = require('inquirer')
 const db = require('./config/connection')
+
+db.connect(err => {
+    if (err) throw err;
+    console.log('Connected to database.');
+    employeeTracker();
+});
+
+function employeeTracker() {
+    inquirer.prompt([{
+        type: 'list',
+        name: 'main',
+        message: 'Choose an option.',
+        choices: [
+            'View Departments',
+            'View Roles',
+            'View Employees',
+            'Add Department',
+            'Add Role',
+            'Add Employee',
+            'Exit'
+        ]
+    }]).then((answers) => {
+        
+        }
+    )
+}
