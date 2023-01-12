677aac44142b2815757877be99bac9ca94439cd6
CL <cl.czayas@gmail.com>
2023-01-12 19:25:31 -0400

    Finished inquiries & schema

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,9 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
+    port: 8889,
     password: 'root',
     database: 'employee_db'
 })
--- db/schema.sql
+++ db/schema.sql
@@ -7,14 +7,14 @@ USE employee_db;
 CREATE TABLE department (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30)
-)
+);
 
 CREATE TABLE role (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT
-)
+);
 
 CREATE TABLE employee (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -22,4 +22,4 @@ CREATE TABLE employee (
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT
-)
+);
